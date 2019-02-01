4fd97d25d411a473490dfec51bdb104fb59b5d3d
harsh-not-haarsh <44001232+harsh-not-haarsh@users.noreply.github.com>
2019-02-01 11:05:00 +0530

    this is the batch file to install all your pre-requisites

---
+++ batchfile.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+ECHO.
+ECHO This is a programme that will install all the necessary libraries required
+pip install geocoder
+pip install webbrowser
+pip install pynput
+pip install smtplib
+pip install ssl
+ECHO.
+PAUSE
+CLS
+EXIT
